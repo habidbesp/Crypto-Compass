874b865d9931b2d62698b04c158e021a4bbd1095
Habid B Espinosa <hab.bad@hotmail.com>
2024-10-12 12:22:18 +0200

    first commit, files in src: components, data, types

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="app-title">
+          <span className="">Crypto Compass</span>A Cryptocurrency Price Tracker
+        </h1>
+
+        <div className="content"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
